74b3dcba7a8110a90ce0d075f93280860a1cb8b2
CesarAugustoJoaquim <cesar.a.joaquim@gmail.com>
2021-05-05 15:12:56 -0300

    CRUD atividade
    Atividade controller;
    Atividade schema;
    Routes ativadade.

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {funcController} from "../controllers/funcController";
+import { AtividadeController } from "../controllers/AtividadeController";
 
 const router = Router();
 const funcionario = new funcController();
+const atividade = new AtividadeController();
 
 router.post("/cadastrar/funcionario",funcionario.cadastrar);
 router.post("/buscar/funcionario", funcionario.buscaId);
@@ -10,6 +12,11 @@ router.post("/buscar/funcionario/:nome", funcionario.buscaNome);
 router.post("/funcionario/alterar",funcionario.alterar);
 router.post("/funcionario/remover",funcionario.remover);
 
+router.post("/ativiade/cadastrar", atividade.cadastrar);
+router.get("/atividade/procurar/:titulo", atividade.procurarAtividadePorTitulo);
+router.get("/atividade/procurar/:criador", atividade.procurarAtividadePorCriador);
+router.put("/atividade/alterar", atividade.alterar);
+router.delete("/atividade/deletar/:titulo", atividade.deletar);
 
 
 
